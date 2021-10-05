54dc2ba28a9e73c2a8cc8876466bfdac854c8048
Kilian Lieret <kilian.lieret@posteo.de>
2021-10-05 11:54:23 +0200

    Extract code into functions

--- armor_interaction.py
+++ armor_interaction.py
@@ -1,15 +1,18 @@
+#!/usr/bin/env python3
+
 import pandas as pd
 import collections
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 import yaml
 from pathlib import Path
 
-df = pd.read_csv("data/armor_weapon_interaction.csv")
 
-with Path("data/predefined_armor_pieces.yaml").open() as inf:
-    _our_armor = yaml.load(inf, Loader=yaml.SafeLoader)
-our_armor = {armor["name"]: armor for armor in _our_armor}
+def get_predefined_armor(path=Path("data/predefined_armor_pieces.yaml")):
+    with path.open() as inf:
+        _armor = yaml.load(inf, Loader=yaml.SafeLoader)
+    return {armor["name"]: armor for armor in _armor}
 
+our_armor = get_predefined_armor()
 
 def resolve_layer(layer_code: str):
     if layer_code.endswith("2"):
@@ -18,21 +21,26 @@ def resolve_layer(layer_code: str):
         return layer_code, 1
 
 
-def get_armor_layer(name: str, body_part="default"):
-    armor = our_armor[name]
+def get_armor_layer(armor_dict, name: str, body_part="default"):
+    armor = armor_dict[name]
     if body_part not in armor["armor"]:
         body_part = "default"
     return [resolve_layer(layer) for layer in armor["armor"][body_part]]
 
 
-def get_armor_layers(names: List[str], body_part="default"):
+def get_armor_layers(armor_dict, names: List[str], body_part="default"):
     layers = []
     for name in names:
-        layers.extend(get_armor_layer(name, body_part=body_part))
+        layers.extend(get_armor_layer(armor_dict, name, body_part=body_part))
     return layers
 
 
-armor_weapon_interaction = {tuple(x[:-1]): x[-1] for x in df.to_numpy()}
+def get_armor_weapon_interaction_dict(path=Path("data/armor_weapon_interaction.csv")) -> Dict[Tuple[str, str, int], int]:
+    df = pd.read_csv(path)
+    return {tuple(x[:-1]): x[-1] for x in df.to_numpy()}
+
+
+armor_weapon_interaction = get_armor_weapon_interaction_dict()
 
 
 def get_damage(damage: int, damage_type: str, penetration: int, armor_layers: List[Tuple[str, int]]):
@@ -62,6 +70,7 @@ print(get_damage(
     "p", 
     8, 
     get_armor_layers(
+        our_armor,
         [
             "Helmar's Shield of Meginbald",
             "Helmar's Warrior Priest Armour", 
