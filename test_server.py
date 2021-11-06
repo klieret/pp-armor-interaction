de8f2f6162a3c71918f491b74e74166aabbba612
Kilian Lieret <kilian.lieret@posteo.de>
2021-11-06 12:26:01 +0100

    ♻️ Use general toggle_armor and set_int_slider

--- test_server.py
+++ test_server.py
@@ -41,20 +41,18 @@ def get_input_damage(driver) -> str:
     return driver.find_element_by_id("input_damage").get_property("value")
 
 
-def toggle_helmars_warrior_priest_armour(driver) -> None:
-    driver.find_element_by_id(
-        "armor_selection_Helmar's Warrior Priest Armour"
-    ).click()
+def toggle_armor(driver, armor: str) -> None:
+    driver.find_element_by_id(f"armor_selection_{armor}").click()
 
 
 def select_damage_type(driver, damage_type: str) -> None:
     driver.find_element_by_id(f"damage_type_{damage_type}").click()
 
 
-def set_penetration(driver, penetration: int) -> None:
-    slider = driver.find_element_by_id("input_penetration")
+def set_int_slider(driver, slider_name: str, target: int) -> None:
+    slider = driver.find_element_by_id(slider_name)
     now = int(slider.get_property("value"))
-    difference = now - penetration
+    difference = now - target
     if difference == 0:
         return
     for i in range(int(fabs(difference))):
@@ -62,15 +60,32 @@ def set_penetration(driver, penetration: int) -> None:
             slider.send_keys(Keys.RIGHT)
         else:
             slider.send_keys(Keys.LEFT)
-    assert int(slider.get_property("value")) == penetration
+    assert int(slider.get_property("value")) == target
+
+
+def set_penetration(driver, penetration: int) -> None:
+    set_int_slider(
+        slider_name="input_penetration", driver=driver, target=penetration
+    )
+
+
+def set_input_damage(driver, damage: int) -> None:
+    set_int_slider(slider_name="input_damage", driver=driver, target=damage)
+
+
+def test_set_input_damage(session) -> None:
+    for value in [1, 5, 10, 15]:
+        set_input_damage(session, value)
+        assert get_input_damage(session) == str(value)
 
 
 def test_helmar(session):
-    toggle_helmars_warrior_priest_armour(session)
+    h = "Helmar's Warrior Priest Armour"
+    toggle_armor(session, h)
     assert get_result(session) == "0"
-    toggle_helmars_warrior_priest_armour(session)
+    toggle_armor(session, h)
     assert get_result(session) == "10"
-    toggle_helmars_warrior_priest_armour(session)
+    toggle_armor(session, h)
     set_penetration(session, 3)
     assert get_result(session) == "2"
 
